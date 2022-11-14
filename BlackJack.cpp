35de75a50f2e3148d88f42c9690698e35922d734
ctn <35670719+nguyenchloet@users.noreply.github.com>
2022-11-14 12:50:29 -0500

    GenericPlayer class

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -134,3 +134,45 @@ int Hand::GetTotal() const {
     return total;
 
 }
+
+// GENERIC PLAYER CLASS
+
+class GenericPlayer : public Hand {
+        friend std::ostream& operator<<(std::ostream& os, const GenericPlayer& aGenericPlayer);
+
+    public: 
+        GenericPlayer(const std::string& name = " ");
+
+        virtual ~GenericPlayer();
+
+        //indicates whether or not generic player wants to keep hitting
+        virtual bool IsHitting() const = 0;
+
+        //returns whether generic player has busted - has a total greater than 21
+        bool IsBusted() const;
+
+        //announces that the generic player busts
+        void Bust() const;
+
+    protected:
+        std::string m_Name;
+};
+
+// constructor outside of the class 
+GenericPlayer::GenericPlayer(const std::string& name):
+    m_Name(name)
+{}
+
+// destructor outside of class
+GenericPlayer::~GenericPlayer() {}
+
+// check if total is greater than 21
+bool GenericPlayer::IsBusted() const {
+    return (GetTotal() > 21);
+}
+
+// announce name of player busted
+void GenericPlayer::Bust() const {
+    std::cout << "Player " << m_Name << " busts. " << std::endl;
+}
+
