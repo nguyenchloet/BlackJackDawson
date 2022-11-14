f1470035b235a5264500748bbb8ee833d4eefa78
ctn <35670719+nguyenchloet@users.noreply.github.com>
2022-11-14 12:48:21 -0500

    Hand Class

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -52,3 +52,85 @@ int Card::GetValue() const {
 void Card::Flip() {
     m_IsFaceUp = !(m_IsFaceUp);
 }
+
+// HAND CLASS
+
+class Hand {
+    public:
+        //constructor
+        Hand();
+        //destructor
+        virtual ~Hand();
+
+        // adds a card to the hand
+        void Add(Card* pCard);
+
+        // clears hand of all cards
+        void Clear();
+
+        //gets hand total value, intelligently treats aces as 1 or 11
+        int GetTotal() const;
+
+    protected:
+        std::vector<Card*> m_Cards;
+};
+
+Hand::Hand() {}
+
+Hand::~Hand() {
+    Clear();
+}
+
+// push hand to end of vector
+void Hand::Add(Card* pCard) {
+    m_Cards.push_back(pCard); 
+}
+
+// empty the entire vector
+void Hand::Clear() {
+    //iterate through vector, freeing all memory on the heap
+    std::vector<Card*>::iterator iter = m_Cards.begin();
+    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter) {
+        delete *iter;
+        *iter = 0;
+    }
+    // clear pointers from vector
+    m_Cards.clear();
+}
+
+int Hand::GetTotal() const {
+    // if no cards in hand, return 0
+    if (m_Cards.empty()) {
+        return 0;
+    }
+    
+    //if a first card has value of 0, then card is face down; return 0
+    if (m_Cards[0]->GetValue() == 0) {
+        return 0;
+    }
+   
+    //add up card values, treat each ace as 1
+    int total = 0;
+    std::vector<Card*>::const_iterator iter;
+    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter ) {
+        total+= (*iter)->GetValue();
+    }
+    
+    // determine if hand contains an ace
+    bool containsAce = false;
+    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter) {
+        if ((*iter)->GetValue() == Card::ACE ) {
+            containsAce = true;
+        }
+    }
+
+    //if hand contains ace and total is low enough, treat ace as 11
+    // ACE card can be either 1 or 11 in value (initial ACE value is 1, thus if used as 11, ten is added)
+    if (containsAce && total < 10) {
+        total+=10;
+    }
+    
+    // else ACE will remain value 1
+    return total;
+
+}
